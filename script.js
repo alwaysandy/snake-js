771501ddaa2a9fffae66a61ea1565a318efdb9f3
Andrew Daurio-Sas <andrewdaurio@gmail.com>
2022-02-09 19:13:55 -0500

    First color change attempt

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function populateBoard(board) {
         line.classList.add('line')
         for (let x = 0; x < 30; x++) {
             const tile = document.createElement('div');
-            tile.style.width = "1em";
-            tile.style.height = "1em";
-            tile.style.border = "1px solid black";
+            tile.style.width = "2.5vh";
+            tile.style.height = "2.5vh";
+            tile.style.border = "1px solid #888";
             tile.dataset.x = x;
             tile.dataset.y = y;
             line.appendChild(tile);
