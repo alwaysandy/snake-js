7b0c2780ece25324409f0783345374fcd92f1e73
Andrew Daurio-Sas <andrewdaurio@gmail.com>
2022-02-09 19:08:36 -0500

    Change board size unit to vh and vw

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function populateBoard(board) {
         line.classList.add('line')
         for (let x = 0; x < 30; x++) {
             const tile = document.createElement('div');
-            tile.style.width = "1.5em";
-            tile.style.height = "1.5em";
+            tile.style.width = "2.5vw";
+            tile.style.height = "2.5vh";
             tile.style.border = "1px solid black";
             tile.dataset.x = x;
             tile.dataset.y = y;
